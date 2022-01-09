105dd19137175d91c777bdc92ffc1916a507cd4c
github-action-benchmark <github@users.noreply.github.com>
2022-01-09 14:26:18 +0000

    add OpenTelemetry-cpp Benchmark (googlecpp) benchmark result for 0dc9420f6237d34c733ccfe175fcabcc8b607987

---
+++ benchmarks/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1641738378903,
+  "repoUrl": "https://github.com/esigo/opentelemetry-cpp",
+  "entries": {
+    "OpenTelemetry-cpp Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "71217171+esigo@users.noreply.github.com",
+            "name": "Ehsan Saei",
+            "username": "esigo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0dc9420f6237d34c733ccfe175fcabcc8b607987",
+          "message": "Update benchmark.yml",
+          "timestamp": "2022-01-09T15:20:50+01:00",
+          "tree_id": "8e8a19f1b7339c944e49486b42d62083616e0050",
+          "url": "https://github.com/esigo/opentelemetry-cpp/commit/0dc9420f6237d34c733ccfe175fcabcc8b607987"
+        },
+        "date": 1641738378109,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          }
+        ]
+      }
+    ]
+  }
+}
