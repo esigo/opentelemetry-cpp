5e46842c7a33bc32eba692e21402917bff12f73f
github-action-benchmark <github@users.noreply.github.com>
2022-01-09 10:39:39 +0000

    add C++ Benchmark (googlecpp) benchmark result for 7d6a2fad11fa49f3eeaff8e1cf7ca59d1a3374e4

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641723958217,
+  "lastUpdate": 1641724779506,
   "repoUrl": "https://github.com/esigo/opentelemetry-cpp",
   "entries": {
     "C++ Benchmark": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 19.536700908202576,
             "unit": "ns/iter",
             "extra": "iterations: 35823841\ncpu: 19.536324985363805 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ehsan.sadroid@gmail.com",
+            "name": "esigo",
+            "username": "esigo"
+          },
+          "committer": {
+            "email": "ehsan.sadroid@gmail.com",
+            "name": "esigo",
+            "username": "esigo"
+          },
+          "distinct": true,
+          "id": "7d6a2fad11fa49f3eeaff8e1cf7ca59d1a3374e4",
+          "message": "store all the results",
+          "timestamp": "2022-01-09T11:33:48+01:00",
+          "tree_id": "7e5494967ff69c287255ce8ebad16655c2cd1853",
+          "url": "https://github.com/esigo/opentelemetry-cpp/commit/7d6a2fad11fa49f3eeaff8e1cf7ca59d1a3374e4"
+        },
+        "date": 1641724779090,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_RandomIdGeneration",
+            "value": 28.399918448070505,
+            "unit": "ns/iter",
+            "extra": "iterations: 23526114\ncpu: 28.390944632845017 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_RandomIdStdGeneration",
+            "value": 22.973388613769288,
+            "unit": "ns/iter",
+            "extra": "iterations: 30167876\ncpu: 22.952179331418627 ns\nthreads: 1"
           }
         ]
       }
