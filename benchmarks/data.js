92a0225635ae2e46cdcd41b77c9eb008e7067577
github-action-benchmark <github@users.noreply.github.com>
2022-01-09 14:01:17 +0000

    add C++ Benchmark (googlecpp) benchmark result for f7048fd1b6efd0a41441c82e34c0b47f26e289d2

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641734649275,
+  "lastUpdate": 1641736877316,
   "repoUrl": "https://github.com/esigo/opentelemetry-cpp",
   "entries": {
     "OpenTelemetry-cpp benchmarks": [
@@ -23,6 +23,60 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/esigo/opentelemetry-cpp/commit/28fddc6615abddc2e75870a48510040c65247939"
         },
         "date": 1641734648779,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          }
+        ]
+      }
+    ],
+    "C++ Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "71217171+esigo@users.noreply.github.com",
+            "name": "Ehsan Saei",
+            "username": "esigo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f7048fd1b6efd0a41441c82e34c0b47f26e289d2",
+          "message": "Update benchmark.yml",
+          "timestamp": "2022-01-09T14:55:22+01:00",
+          "tree_id": "264cfc943596de959be940762013934c67912e1b",
+          "url": "https://github.com/esigo/opentelemetry-cpp/commit/f7048fd1b6efd0a41441c82e34c0b47f26e289d2"
+        },
+        "date": 1641736876845,
         "tool": "googlecpp",
         "benches": [
           {
