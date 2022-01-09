8268a6bfeb45a9ae756b54c4c9ae1565460229c5
github-action-benchmark <github@users.noreply.github.com>
2022-01-09 16:55:30 +0000

    add OpenTelemetry-cpp Benchmark (googlecpp) benchmark result for 9269696f628ba624424b501cc742b57687f282cf

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641738378903,
+  "lastUpdate": 1641747330210,
   "repoUrl": "https://github.com/esigo/opentelemetry-cpp",
   "entries": {
     "OpenTelemetry-cpp Benchmark": [
@@ -23,6 +23,58 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/esigo/opentelemetry-cpp/commit/0dc9420f6237d34c733ccfe175fcabcc8b607987"
         },
         "date": 1641738378109,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "unit": "undefined/iter",
+            "extra": "iterations: undefined\ncpu: undefined undefined\nthreads: undefined"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ehsan.sadroid@gmail.com",
+            "name": "esigo",
+            "username": "esigo"
+          },
+          "committer": {
+            "email": "ehsan.sadroid@gmail.com",
+            "name": "esigo",
+            "username": "esigo"
+          },
+          "distinct": true,
+          "id": "9269696f628ba624424b501cc742b57687f282cf",
+          "message": "use bazel",
+          "timestamp": "2022-01-09T17:49:42+01:00",
+          "tree_id": "b709932a5e935f6e53d9423864ebeaa2b0c0bd0b",
+          "url": "https://github.com/esigo/opentelemetry-cpp/commit/9269696f628ba624424b501cc742b57687f282cf"
+        },
+        "date": 1641747329404,
         "tool": "googlecpp",
         "benches": [
           {
