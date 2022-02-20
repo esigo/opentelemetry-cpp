3c118f11fe5db43d482fde7e07f9bb9f8598def3
Oblivion <ehsan.sadroid@gmail.com>
2022-02-20 20:44:52 +0000

    install boost vcpkg

--- ci/setup_thrift.ps1
+++ ci/setup_thrift.ps1
@@ -11,6 +11,6 @@ setx VCPKG_DIR (Get-Item -Path ".\").FullName
 
 # boost needed for thrift
 #./vcpkg install boost-locale[core]:x64-windows boost-numeric-conversion[core]:x64-windows boost-scope-exit[core]:x64-windows  openssl:x64-windows
-./vcpkg install  openssl:x64-windows
+./vcpkg install  openssl:x64-windows boost-numeric-conversion[core]:x64-windows
 
 Pop-Location
