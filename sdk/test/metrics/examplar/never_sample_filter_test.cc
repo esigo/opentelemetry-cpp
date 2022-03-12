8ed827c3eeaaf9dd20eabc124fede8a80df4aaaa
Oblivion <ehsan.sadroid@gmail.com>
2022-03-12 20:48:31 +0000

    NeverSampleFilter test

---
+++ sdk/test/metrics/examplar/never_sample_filter_test.cc
@@ -0,0 +1,19 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef ENABLE_METRICS_PREVIEW
+#  include "opentelemetry/sdk/metrics/examplar/never_sample_filter.h"
+#  include <gtest/gtest.h>
+
+using namespace opentelemetry::sdk::metrics;
+
+TEST(SampleMeasurement, shouldSampleMeasurement)
+{
+  auto filter = opentelemetry::sdk::metrics::NeverSampleFilter::GetNeverSampleFilter();
+  ASSERT_TRUE(!filter->shouldSampleMeasurement(1.0, MetricAttributes{},
+                                               opentelemetry::trace::SpanContext{false, false}));
+  ASSERT_TRUE(!filter->shouldSampleMeasurement(1l, MetricAttributes{},
+                                               opentelemetry::trace::SpanContext{false, false}));
+}
+
+#endif
