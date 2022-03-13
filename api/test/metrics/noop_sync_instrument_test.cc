61ae1e4d3db9854a9e45ab5c6d1b6271768ce773
Ehsan Saei <71217171+esigo@users.noreply.github.com>
2022-03-13 16:06:39 +0100

    fix test

--- api/test/metrics/noop_sync_instrument_test.cc
+++ api/test/metrics/noop_sync_instrument_test.cc
@@ -25,9 +25,9 @@ TEST(histogram, Record)
       new opentelemetry::metrics::NoopHistogram<long>("test", "none", "unitless")};
 
   std::map<std::string, std::string> labels = {{"k1", "v1"}};
-  EXPECT_NO_THROW(counter->Record(10l, labels));
-  EXPECT_NO_THROW(counter->Record(2l));
-  EXPECT_NO_THROW(counter->Record(10l, {{"k1", "1"}, {"k2", 2}}));
+  EXPECT_NO_THROW(counter->Record(10l, labels, opentelemetry::context::Context{}));
+  EXPECT_NO_THROW(counter->Record(2l, opentelemetry::context::Context{}));
+  EXPECT_NO_THROW(counter->Record(10l, {{"k1", "1"}, {"k2", 2}}, opentelemetry::context::Context{}));
 }
 
 TEST(UpDownCountr, Record)
