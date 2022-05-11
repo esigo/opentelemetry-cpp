c7a7e70c9dd0b3d0a5d01d1a213b67417ab18e0b
Oblivion <ehsan.sadroid@gmail.com>
2022-05-11 16:55:06 +0000

    fix

--- cmake/opentelemetry-proto.cmake
+++ cmake/opentelemetry-proto.cmake
@@ -136,11 +136,7 @@ if(WITH_OTLP_GRPC)
     set("ENV{LD_LIBRARY_PATH}" "/workspace/grpc/build/stage/lib")
     set("ENV{PATH}" "/workspace/grpc/build/stage/bin")
     execute_process(COMMAND 
-      ${PROTOBUF_PROTOC_EXECUTABLE} --proto_path=${PROTO_PATH} ${PROTOBUF_INCLUDE_FLAGS}
-      --cpp_out=${GENERATED_PROTOBUF_PATH}
-      --grpc_out=generate_mock_code=true:${GENERATED_PROTOBUF_PATH}
-      --plugin=protoc-gen-grpc=${gRPC_CPP_PLUGIN_EXECUTABLE}
-      ${proto_file}
+      ${PROTOBUF_PROTOC_EXECUTABLE} --proto_path=${PROTO_PATH} ${PROTOBUF_INCLUDE_FLAGS} --cpp_out=${GENERATED_PROTOBUF_PATH} --grpc_out=generate_mock_code=true:${GENERATED_PROTOBUF_PATH} --plugin=protoc-gen-grpc=${gRPC_CPP_PLUGIN_EXECUTABLE} ${proto_file}
     )
   endforeach()
 else()
