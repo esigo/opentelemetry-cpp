e0014c32c922e8b45242a042f40795eb3053e34f
Oblivion <ehsan.sadroid@gmail.com>
2022-11-23 22:27:45 +0000

    fix

--- ci/run_docker.sh
+++ ci/run_docker.sh
@@ -5,9 +5,19 @@
 
 set -e
 
+format="${FORMAT:=OFF}"
+thrift="${THRIFT:=OFF}"
+abseil="${ABSEIL:=OFF}"
+gRPC="${GRPC:=OFF}"
+
 BUILD_IMAGE=opentelemetry-cpp-build-2004
 docker image inspect "$BUILD_IMAGE" &> /dev/null || {
-  docker build -t "$BUILD_IMAGE" ci -f ci/Dockerfile.ubuntu.2004
+  docker build -t "$BUILD_IMAGE" \
+    --build-arg FMT=${format} \
+    --build-arg THFT=${thrift} \
+    --build-arg ABSL=${abseil} \
+    --build-arg GRPC=${gRPC} \
+    ci -f ci/Dockerfile.ubuntu.2004
 }
 
 if [[ $# -ge 1 ]]; then
