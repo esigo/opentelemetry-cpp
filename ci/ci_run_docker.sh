2da1b155f976730157864c8f60d95fb5849ff909
Oblivion <ehsan.sadroid@gmail.com>
2022-12-03 17:10:32 +0000

    build image only

--- ci/ci_run_docker.sh
+++ ci/ci_run_docker.sh
@@ -3,31 +3,89 @@
 # Copyright The OpenTelemetry Authors
 # SPDX-License-Identifier: Apache-2.0
 
-set -e
+set -o errexit
+set -o nounset
+set -o pipefail
 
 format="${FORMAT:=OFF}"
 thrift="${THRIFT:=OFF}"
 abseil="${ABSEIL:=OFF}"
 gRPC="${GRPC:=OFF}"
 bazel="${BAZEL:=OFF}"
-
 BUILD_IMAGE=opentelemetry-cpp-build-2004
-docker image inspect "$BUILD_IMAGE" &> /dev/null || {
-  docker build -t "$BUILD_IMAGE" \
-    --build-arg FMT=${format} \
-    --build-arg THFT=${thrift} \
-    --build-arg ABSL=${abseil} \
-    --build-arg GRPC=${gRPC} \
-    --build-arg BZL=${bazel} \
-    --build-arg BASE=ubuntu:20.04 \
-    ci -f ci/Dockerfile.ubuntu
+docker rmi --force ${BUILD_IMAGE}
+
+Help()
+{
+   # Display Help
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: scriptTemplate [-b|j]"
+   echo "options:"
+   echo "i     Build image"
+   echo "b     With bazel"
+   echo "f     With format"
+   echo "t     With thrift"
+   echo "a     With abseil"
+   echo "g     With gRPC"
+   echo "h     Print Help."
+   echo "j     Parallel jobs"
+   echo
 }
+
+build_image()
+{
+  docker image inspect "$BUILD_IMAGE" &> /dev/null || {
+    docker build -t "$BUILD_IMAGE" \
+      --build-arg FMT=${format} \
+      --build-arg THFT=${thrift} \
+      --build-arg ABSL=${abseil} \
+      --build-arg GRPC=${gRPC} \
+      --build-arg BZL=${bazel} \
+      --build-arg BASE=ubuntu:20.04 \
+      ci -f ci/Dockerfile.ubuntu
+  }
+}
+
+while getopts ":iftagbj:" option; do
+   case $option in
+    h) # display Help
+         Help
+         exit;;
+    i) # build image
+        build_image
+        exit;;
+    f)
+      format=ON
+      ;;
+    t)
+      thrift=ON
+      ;;
+    a)
+      abseil=ON
+      ;;
+    g)
+      gRPC=ON
+      ;;
+    b)
+      bazel=ON
+      ;;
+    j) # number of cores
+        cores=$OPTARG
+        ;;
+    \?)
+        Help
+        exit;;
+   esac
+done
 
 BAZEL_CACHE="/home/runner/.cache/bazel"
 
 command=$@
 backup="/backup/backup.tar"
 container_name="otel-run"
+
+build_image
 
 if [[ ${bazel} == "ON" ]];
 then
